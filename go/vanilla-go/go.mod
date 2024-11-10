958cce470e1879569610435259fb25f4c49d338f
dezly-macauley <dezlymacauley@proton.me>
2024-11-10 17:31:20 +0200

    Created training repos for Rust and Go. Updated README page

---
+++ go/vanilla-go/go.mod
@@ -0,0 +1,3 @@
+module vanilla-go
+
+go 1.22.6
---
+++ rust/vanilla-rust/target/debug/deps/vanilla_rust-caca72af45d67369.d
@@ -0,0 +1,5 @@
+/home/dezly-macauley/dezly-training-labs/rust/vanilla-rust/target/debug/deps/libvanilla_rust-caca72af45d67369.rmeta: src/main.rs
+
+/home/dezly-macauley/dezly-training-labs/rust/vanilla-rust/target/debug/deps/vanilla_rust-caca72af45d67369.d: src/main.rs
+
+src/main.rs:
