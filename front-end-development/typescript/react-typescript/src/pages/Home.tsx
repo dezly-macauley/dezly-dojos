a4346528af2ddb709382ce99d1ca288f1c99394b
dezly-macauley <dezlymacauley@gmail.com>
2024-11-27 18:47:31 +0200

    Last update before evening session

--- front-end-development/typescript/react-typescript/src/pages/Home.tsx
+++ front-end-development/typescript/react-typescript/src/pages/Home.tsx
@@ -18,6 +18,26 @@ function Home() {
                {/* 
                     NOTE: .map()
 
+                    This is a method in TypeScript that can be used on arrays.
+                    It goes through each element in the array, performs some
+                    type of transformation on each element, and then it 
+                    returns a new array that contains the 
+                    transformed elements.
+
+                    In this case the array is `movies`. Each element in the
+                    array is a an object that represents a movie.
+
+                    Each movie object follows this structure:
+                    {id: number, title: string, release_date: string}
+
+                    .map() will then perform a transformation.
+                    For each object in the array, .map() will return an 
+                    element, specifically a React component.
+
+                    It will return the MovieCard React component, 
+                    but it will use the contents of the movie object to fill
+                    in the prop.
+
                */}
 
                 {movies.map((movie) => (
